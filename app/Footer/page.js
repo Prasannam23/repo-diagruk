3e5e90325d2e99711c7e73086e0e3cb5e7f21bfe
Prasannam <Prasannamtiwari23@gmail.com>
2024-03-03 21:35:51 +0530

    again

--- app/Footer/page.js
+++ app/Footer/page.js
@@ -7,7 +7,8 @@ const Footer = () => {
         <div className="text-center md:w-9/12 md:mr-8">
           <h1 className="text-purple-600 my-6 font-bold text-4xl mb-4 md:text-5xl">Ready to Get Started?</h1>
           <h2 className='text-lg md:text-xl'>"Elevate excellence — where precision meets potential, and success is standard."</h2>
-          <button className="bg-white my-8 h-10 w-40 text-purple-600 font-bold py-2 px-4 rounded md:w-auto md:py-3 md:px-6">Contact Us</button>
+          <Link href="/Contact_US" className='my-2 text-1xl'>
+            <button className="bg-white my-8 h-10 w-40 text-purple-600 font-bold py-2 px-4 rounded md:w-auto md:py-3 md:px-6">Contact Us</button></Link>
         </div>
         <div className='md:w-3/12 mt-8 md:mt-0'>
           <div className='flex flex-col text-center justify-center text-color-white'>
@@ -17,7 +18,7 @@ const Footer = () => {
             </Link>
             <Link href="/" className='my-2 text-1xl'>Courses
             </Link>
-            <Link href="/" className='my-2 text-1xl'>About Us
+            <Link href="/About_us" className='my-2 text-1xl'>About Us
             </Link>
           </div>
         </div>
